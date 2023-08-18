7cd928a5edce7d14e4df293b0c88f1731abc06fa
saluumaa <salumahassan02@gmail.com>
2023-08-18 15:40:17 +0300

    Create post model class

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+   belongs_to :author, class_name: "User" 
+    has_many :likes, dependent: :destroy
+    has_many :comments, dependent: :destroy
+
+    after_save :update_posts_counter
+
+    def update_posts_counter
+        author.increment!(:PostsCounter)
+    end
+
+    def five_recent_comments
+        comments.order(created_at: :desc).limit(5)
+    end
+
+
+end

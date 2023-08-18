38abf3ddae6eec72186ad3f11d4b621a3a92beb6
saluumaa <salumahassan02@gmail.com>
2023-08-18 16:27:18 +0300

    Create comment model

---
+++ app/models/comment.rb
@@ -0,0 +1,10 @@
+class Comment < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  after_create :increment_comments_counter
+
+  def increment_comments_counter
+    post.increment!(:comments_counter)
+  end
+end

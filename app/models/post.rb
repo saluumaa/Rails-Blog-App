8935180503cfa40bfb4a3f77c7e40e1777343bd4
saluumaa <salumahassan02@gmail.com>
2023-08-18 21:52:42 +0300

    Add validations to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes, dependent: :destroy
-  has_many :comments, dependent: :destroy
+  has_many :likes
+  has_many :comments
 
   after_save :update_posts_counter
 
+  validates :text, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def update_posts_counter
-    author.increment!(:PostsCounter)
+    author.increment!(:posts_counter)
   end
 
   def five_recent_comments

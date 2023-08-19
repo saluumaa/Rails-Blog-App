f0e803cd255c4508e48ebbb9c3f7b56a728ad638
saluumaa <salumahassan02@gmail.com>
2023-08-19 17:35:38 +0300

    fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: "User"
-    belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
 
-    after_create :increment_comments_counter
+  after_create :increment_comments_counter
 
-    def increment_comments_counter
-        post.increment!(:comments_counter)
-    end
+  def increment_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,15 @@
 class Post < ApplicationRecord
-   belongs_to :author, class_name: "User" 
-    has_many :likes, dependent: :destroy
-    has_many :comments, dependent: :destroy
+  belongs_to :author, class_name: 'User'
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
-    after_save :update_posts_counter
+  after_save :update_posts_counter
 
-    def update_posts_counter
-        author.increment!(:PostsCounter)
-    end
+  def update_posts_counter
+    author.increment!(:PostsCounter)
+  end
 
-    def five_recent_comments
-        comments.order(created_at: :desc).limit(5)
-    end
-
-
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end

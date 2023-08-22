2287d69832fff6978957095193195f0013a53f01
saluumaa <salumahassan02@gmail.com>
2023-08-22 19:02:28 +0300

    Add users spec test for users controller

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,17 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    it 'returns http success' do
-      get '/users/index'
-      expect(response).to have_http_status(:success)
-    end
+ describe 'GET /index' do
+  it 'renders a successful response' do
+    get users_path
+    expect(response).to be_successful
   end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
-      expect(response).to have_http_status(:success)
-    end
+  it 'ensure correct template is rendered' do
+    get users_path
+    expect(response).to render_template('users/index')
   end
+
+  it 'includes correct placeholder text' do
+    get users_path
+    expect(response.body).to include('<p> Here is a list of users: </p>')
+  end
+end
+
+  describe 'GET /users/:id' do
+    let (:user) { User.create!(name: 'Salma Hassan', bio: 'This is my bio') }
+    it 'renders a successful response' do
+      get user_path(user)
+      expect(response).to be_successful
+    end
+
+    it 'ensure correct template is rendered' do
+      get user_path(user)
+      expect(response).to render_template('users/show')
+    end
+
+    it 'includes correct placeholder text' do
+      get user_path(user)
+      expect(response.body).to include('<p> Here is a list of user by id </p>')
+    end
+end
 end

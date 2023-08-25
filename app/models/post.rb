a3e810ff8f016679ec87d1ce404cfcdf9bd1f232
saluumaa <salumahassan02@gmail.com>
2023-08-25 23:20:16 +0300

    Update post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,7 +10,7 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_posts_counter
-    author.increment!(:posts_counter)
+    author.increment!(:PostsCounter)
   end
 
   def five_recent_comments

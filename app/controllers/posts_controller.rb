bad74e888d9ed00eab9d3f2ee60d16442ac49710
saluumaa <salumahassan02@gmail.com>
2023-08-25 23:14:33 +0300

    Add create and new for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,32 @@ class PostsController < ApplicationController
   end
 
   def show
-    @posts = Post.find(params[:id])
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+    @like = Like.new
   end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @user = current_user
+    @post = current_user.posts.build(post_params)
+    @post.author_id = @user.id
+
+    if @post.save
+      redirect_to user_posts_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end

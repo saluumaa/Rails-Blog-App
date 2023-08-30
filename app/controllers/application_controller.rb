c99384d415f4d026948b0cebc8d2632e1cb266f0
saluumaa <salumahassan02@gmail.com>
2023-08-30 16:08:23 +0300

    Generate devise and views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_user
-    User.first
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
